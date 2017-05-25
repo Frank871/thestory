f255fa578f2456d845b7e81aa7046bffa3b89f20
Franklin Segura <franklinsegura@Franklins-MacBook-Air.local>
2017-05-25 09:34:06 -0400

    changes made working now

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect((process.env.MONGODB_URI ||'mongodb://localhost/mongoscraper'));
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -46,18 +46,18 @@ db.once("open", function() {
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  request("http://drudgereport.com/", function(error, response, html) {
+  request("https://docs.mongodb.com/manual/crud/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
-    $("div.app_topstories ul li").each(function(i, element) {
+    $("li").each(function(i, element) {
 
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("h3.title").text();
-      result.link = "http://drudgereport.com" + $(this).children("h3.title").children("a").attr("href");
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
 
       // Using our Article model, create a new entry
       // This effectively passes the result object to the entry (and the title and link)
